57860549dca2e4afe6183e57ff7fe9a3f910eb65
rajat <sharmarajat532@gmail.com>
2024-05-21 01:37:59 +0530

    Added alert button

--- my-react-app/src/components/Alert.jsx
+++ my-react-app/src/components/Alert.jsx
@@ -6,7 +6,7 @@ const Alert = (props) => {
         <>
             <div className={`alert alert-${props.alert.alertType} alert-dismissible fade show`} role="alert">
                 <strong>{props.alert.alertMessage} </strong>{props.alert.message}
-                {/*<button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>*/}
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         </>
     );
